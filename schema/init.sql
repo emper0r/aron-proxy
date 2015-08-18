7c8d32de46f893feecc8fbb6119924d76e3f0303
Tony Peña <antonio@ctime.it>
2015-08-19 00:07:05 +0200

    * Change field on init schema

--- schema/init.sql
+++ schema/init.sql
@@ -3,8 +3,7 @@ CREATE TABLE `License_license` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `client` varchar(64) NOT NULL,
     `province` varchar(64) NOT NULL,
-    `lic_a` varchar(64) NOT NULL,
-    `lic_b` varchar(64) NOT NULL,
-    `exp_lic` date NOT NULL
-);
+    `req` varchar(64) NOT NULL,
+    `lic` varchar(64) NOT NULL,
+    `exp_lic` date NOT NULL);
 COMMIT;
