636a22007b8d08b62373f48c0b66fc3cbfe98be8
Tony Peña <antonio@ctime.it>
2015-08-28 10:59:05 +0200

    * Fixed URL to see site directly without /admin

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 admin.site.site_header = 'Aron Web Manager'
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(admin.site.urls)),
 )
